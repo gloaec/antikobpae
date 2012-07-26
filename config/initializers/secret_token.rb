13d372b37af2d7220f745fd938d8907c5db6e3b0
Ghislain Loaec <ghis182@gmail.com>
2012-07-26 14:52:41 +0700

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Antikobpae::Application.config.secret_token = '8dbe6be4fd7efd602d74cb3ffba9d53e565e460a2fab569803a7d7237b5ea59f2ab2be69445561c8d86fab7c1b0fa4a65cf8948356891f6873b7a491ab32e379'
